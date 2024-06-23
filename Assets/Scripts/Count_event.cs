6767ba309c099567f28af852e86d4fdc3d9d6399
MATU <132588328+Matyy1975@users.noreply.github.com>
2024-06-23 18:06:39 -0400

    UI DE GAMEPLAY
    Hay ui de gameplay u un panel de mision para pasar el nivel

--- Assets/Scripts/Count_event.cs
+++ Assets/Scripts/Count_event.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
+using TMPro;
 public class Count_event : MonoBehaviour
 {
+
     // Start is called before the first frame update
     public int Contador_Global;
+    public int contador_Objetivo;
     public UnityEvent ACTIVAR_EVENTO;
+    public TextMeshProUGUI Contador_texto;
 
-
+    private void Start()
+    {
+        Contador_texto.text = Contador_Global.ToString() +"/" + contador_Objetivo.ToString();
+    }
     public void Restar_Contador()
     {
         Contador_Global--;
@@ -18,7 +24,9 @@ public class Count_event : MonoBehaviour
 
     public void verificar_contador()
     {
-        if (Contador_Global == 0)
+        Contador_texto.text = Contador_Global.ToString() + "/" + contador_Objetivo.ToString();
+
+        if (Contador_Global <= contador_Objetivo)
         {
             ACTIVAR_EVENTO.Invoke();
         }
