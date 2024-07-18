b6e3a1a7d2d06f73fa137b6111c8b5ad6385b2f9
MATU <132588328+Matyy1975@users.noreply.github.com>
2024-07-18 00:54:14 -0400

    Transiciones de audio
    Se arreglaron y no hay error de fadeout o fadein.
    
    LAS transiciones son de 3 segundos en momento de gameplay, eso incluye
    
    Cambio de escenas y Cambio de volumen

--- Assets/Scripts/Selector de niveles/LevelManager.cs
+++ Assets/Scripts/Selector de niveles/LevelManager.cs
@@ -22,14 +22,14 @@ public class LevelManager : MonoBehaviour
         }
 
         // Inicializa los niveles; el primer nivel está desbloqueado por defecto
-        levelsUnlocked = new bool[10];
+        //levelsUnlocked = new bool[7];
         levelsUnlocked[0] = true;
 
         // Cargar el progreso del jugador
         LoadProgress();
     }
 
-    // Método para desbloquear un nivel
+    // Método para desbloquear un nivel específico
     public void UnlockLevel(int levelIndex)
     {
         if (levelIndex >= 0 && levelIndex < levelsUnlocked.Length)
@@ -37,6 +37,16 @@ public class LevelManager : MonoBehaviour
             levelsUnlocked[levelIndex] = true;
             SaveProgress();
         }
+    }
+
+    // Método para desbloquear todos los niveles
+    public void UnlockAllLevels()
+    {
+        for (int i = 0; i < levelsUnlocked.Length; i++)
+        {
+            levelsUnlocked[i] = true;
+        }
+        SaveProgress();
     }
 
     // Método para guardar el progreso del jugador
